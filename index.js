ffe50d4f5f711f6c4b418efd82906abc8f931292
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-06-26 19:39:37 +0530

    Creating a layout

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// Use Express Router
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+// Use Express ejs Layouts
+app.use(expressLayouts);
+
+app.use('/', require('./routers'));
+
+// Setting Up View Engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static('assets'));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(port, function(err){
+
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is up and running on port: ${port}`);
+
+});
