9f0ddf73f418f1c515ec099ed4fb0deedca1fe3a
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-06-27 17:49:46 +0530

    Setting_Up_Static_Files_Access

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+// Use Express Router
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+
+app.use(express.static('./assets'));
+
+// Use Express ejs Layouts
+app.use(expressLayouts);
+
+app.use('/', require('./routers'));
+
+// Setting Up View Engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static('assets'));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(port, function(err){
+
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is up and running on port: ${port}`);
+
+});
