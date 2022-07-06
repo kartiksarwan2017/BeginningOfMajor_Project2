a18d52df45d0b6652ecf1b1f6966c6151f1d0998
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-07-06 23:26:37 +0530

    Rendering Pages for SignUp and SignIn

---
+++ controllers/users_controller.js
@@ -0,0 +1,20 @@
+module.exports.profile = function(req, res){
+    res.render('user_profile',{
+        title: "Profile Page"
+    });
+}
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
