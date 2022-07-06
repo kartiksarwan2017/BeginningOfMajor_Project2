3de04ffdd02a5892fb1394939e7caa471a4428da
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-07-06 15:11:49 +0530

    Implementing Partials

---
+++ routers/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+const usersController = require('../controllers/users_controller');
+
+
+console.log('users Router loaded');
+
+
+router.get('/profile', usersController.profile);
+
+
+module.exports = router;
