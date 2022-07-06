05ea6c3c7c9df5abd1ef7e6fea4f3dc21865ecf8
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-07-06 15:15:09 +0530

    Creating A Layout

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const homeController = require('../controllers/home_controller');
-const profileController = require('../controllers/profile_controller');
+
 
 const router = express.Router();
 
 console.log('router loaded');
 router.get('/', homeController.home);
-router.get('/profile', profileController.profile);
+router.use('/users', require('./users'));
 
 
 module.exports = router;
+
