a47e6f6bfe9993d5677425b1061ea57eb3059661
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-07-06 23:32:30 +0530

    Creating And Altering A Cookie

---
+++ routers/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const homeController = require('../controllers/home_controller');
+
+
+const router = express.Router();
+
+console.log('router loaded');
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+
+module.exports = router;
